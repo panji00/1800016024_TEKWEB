c2142005a560e274f5a9fcef630207eb8666818c
panji <panji1800016024@webmail.uad.ac.id>
2020-03-29 03:53:49 +0700

    tekweblanjut

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './Halaman-utama.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tekweb1800016024';
+}
